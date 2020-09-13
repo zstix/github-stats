2e97cbaa78aaf4ead8bc619c6fd010f2882123fe
Zack Stickles <zstickles@newrelic.com>
2020-09-12 23:09:29 -0700

    feat: render burndown chart

--- lib/github_charts.ex
+++ lib/github_charts.ex
@@ -3,11 +3,12 @@ defmodule GithubCharts do
   @height 400
   @padding 40
   @legend_height 60
-  @bar_padding 8
+  @bar_padding 5
 
-  def draw_chart do
-    data = [3, 3, 4, 5, 2]
-
+  # TODO: colors
+  # TODO: title
+  # TODO: legend
+  def draw_chart(%{title: _title, data: data}, :burndown) do
     %{width: @width, height: @height}
     |> Victor.new()
     |> draw_background()
@@ -30,11 +31,13 @@ defmodule GithubCharts do
   defp draw_bars(chart, data) do
     chart_width = @width - @padding * 2
 
+    first = hd(data)
+
     chart_info = %{
       chart_bottom: @height - @padding - @legend_height,
       chart_height: @height - @legend_height - @padding * 2,
       chart_width: chart_width,
-      chart_max: Enum.max(data),
+      chart_max: first.todo + first.in_progress + first.in_review + first.done,
       bar_width: (chart_width - length(data) * @bar_padding * 2) / length(data)
     }
 
@@ -43,16 +46,40 @@ defmodule GithubCharts do
     |> Enum.reduce(chart, &draw_bar(&2, &1, chart_info))
   end
 
-  defp draw_bar(chart, {val, pos}, info) do
-    height = val / info.chart_max * info.chart_height
+  # TODO: points above bars
+  # TODO: dates below bars
+  defp draw_bar(chart, {data, pos}, info) do
     width = info.bar_width
+    x = pos * (width + @bar_padding * 2) + @padding + @bar_padding
+
+    todo_height = data.todo / info.chart_max * (info.chart_height - @padding)
+    in_progress_height = data.in_progress / info.chart_max * (info.chart_height - @padding)
+    in_review_height = data.in_review / info.chart_max * (info.chart_height - @padding)
+
+    todo_bar = %{
+      x: x,
+      y: info.chart_bottom - todo_height,
+      width: width,
+      height: todo_height
+    }
+
+    in_progress_bar = %{
+      x: x,
+      y: info.chart_bottom - todo_height - in_progress_height,
+      width: width,
+      height: in_progress_height
+    }
+
+    in_review_bar = %{
+      x: x,
+      y: info.chart_bottom - todo_height - in_progress_height - in_review_height,
+      width: width,
+      height: in_review_height
+    }
 
     chart
-    |> Victor.add(:rect, %{
-      x: pos * (width + @bar_padding * 2) + @padding + @bar_padding,
-      y: info.chart_bottom - height,
-      width: width,
-      height: height
-    })
+    |> Victor.add(:rect, todo_bar, %{fill: "red"})
+    |> Victor.add(:rect, in_progress_bar, %{fill: "orange"})
+    |> Victor.add(:rect, in_review_bar, %{fill: "teal"})
   end
 end
--- lib/github_stats.ex
+++ lib/github_stats.ex
@@ -1,14 +1,13 @@
 defmodule GithubStats do
-  # TODO: map dates to data set
-  # TODO: produce image
   def burndown(
         repo \\ "docs-website",
         milestone \\ 2,
         start_date \\ "2020-09-03",
-        end_date \\ "2020-09-18"
+        end_date \\ "2020-09-15"
       ) do
     {:burndown, vars: %{repository: repo, milestone: milestone}}
     |> GithubAPI.query_github(start_date, end_date)
+    |> GithubCharts.draw_chart(:burndown)
   end
 end
 
@@ -16,5 +15,5 @@ end
 # [BOT_NAME] [CHART_NAME] [REPO_NAME] ...[OPTIONS]
 #
 # Burndown Example:
-# GHS burndown docs-website 1 2020-09-01 2020-09-15
+# GHS burndown docs-website 2 2020-09-03 2020-09-15
 # Options: [MILESTONE_ID] [START_DATE] [END_DATE]
