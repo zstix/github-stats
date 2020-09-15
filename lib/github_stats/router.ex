d092b48b7b923de8a5d2cc2da6fcf7f9eaa7630e
Zack Stickles <zstickles@newrelic.com>
2020-09-14 18:39:21 -0700

    chore: router cleanup

--- lib/github_stats/router.ex
+++ lib/github_stats/router.ex
@@ -5,31 +5,27 @@ defmodule GithubStats.Router do
   plug(Plug.Parsers, parsers: [:json, :urlencoded], json_decoder: Jason)
   plug(:dispatch)
 
-  # TODO: clean this up
   # TODO: send SVG back
   # TODO: ensure headers are SVG
   get "/burndown" do
-    {code, resp} =
-      cond do
-        validate_request({:burndown, conn.query_params}) -> {200, "Good"}
-        true -> {400, "Bad Response"}
-      end
-
-    send_resp(conn, code, resp)
-
-    # GithubStats.burndown(repo, milestone, start_date, end_date)
-    # send_resp(conn, 200, "world")
+    conn
+    |> get_missing_params(["repo", "milestone", "start_date", "end_date"])
+    |> case do
+      {conn, ""} -> send_resp(conn, 200, "Good")
+      {conn, missing} -> send_resp(conn, 400, "Missing: #{missing}")
+    end
   end
 
   match _ do
     send_resp(conn, 404, "Not Found")
   end
 
-  defp validate_request({:burndown, params}) do
-    validate_request(["repo", "milestone", "start_date", "end_date"], params)
-  end
-
-  defp validate_request(keys, params) do
-    Enum.all?(keys, &Map.has_key?(params, &1))
+  defp get_missing_params(conn, keys) do
+    {
+      conn,
+      keys
+      |> Enum.filter(&(!Map.has_key?(conn.query_params, &1)))
+      |> Enum.join(", ")
+    }
   end
 end
